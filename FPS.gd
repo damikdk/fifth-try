380a8a5867bccd7a419941a74179548c2e9c3d7d
Damik Minnegalimov <damikdk@gmail.com>
2022-01-30 22:59:04 +0300

    Remove acceleration from movements

--- FPS.gd
+++ FPS.gd
@@ -1,19 +1,13 @@
 extends CharacterBody3D
 
-var speed = 17
-const ACCEL_DEFAULT = 14
-const ACCEL_AIR = 12
-
-@onready var accel = ACCEL_DEFAULT
-var gravity = 9.8
+var speed = 6
+var max_speed_vector = Vector3(speed, speed, speed)
 var jump = 5
 
-var cam_accel = 40
 var mouse_sense = 0.1
 
-var direction = Vector3()
-var velocity = Vector3()
-var gravity_vec = Vector3()
+var vertical_vector = Vector3()
+var horizontal_vector = Vector3()
 
 @onready var head = $Head
 @onready var camera = $Head/Camera
@@ -33,26 +27,31 @@ func _process(delta):
 	camera.global_transform = head.global_transform
 		
 func _physics_process(delta):
+	var direction = Vector3()
+	var velocity = Vector3()
+	
 	# get keyboard input
 	var h_rot = transform.basis.get_euler().y
 	var f_input = Input.get_axis("move_forward", "move_backward")
 	var h_input = Input.get_axis("move_left", "move_right")
 
 	direction = Vector3(h_input, 0, f_input).rotated(up_direction, h_rot).normalized()
+	horizontal_vector = direction * speed
 	
 	# jumping and gravity
 	if is_on_floor():
-		accel = ACCEL_DEFAULT
-		gravity_vec = Vector3.ZERO
-	else:
-		accel = ACCEL_AIR
-		gravity_vec += Vector3.DOWN * gravity * delta
+		vertical_vector = Vector3.ZERO
+	else: 
+		vertical_vector += -up_direction * 9.8 * delta
 		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		gravity_vec = up_direction * jump
+		vertical_vector.y += jump 
+	
+	var result_vector = horizontal_vector + vertical_vector
+	result_vector = result_vector.clamp(result_vector, max_speed_vector)
 	
 	# make it move
-	motion_velocity = velocity.lerp(direction * speed, accel * delta) + gravity_vec
+	motion_velocity = result_vector
 	
 	move_and_slide()
 
